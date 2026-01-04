4e5475afbb8e79a647aa5fd0e896b80b0e2c3774
YeeLing <yeeling.wong@hotmail.com>
2026-01-04 21:50:54 +0800

    Add approved alumni screen

--- app/src/main/java/com/eeszen/alumnidirectoryapp/data/repo/AlumniRepo.kt
+++ app/src/main/java/com/eeszen/alumnidirectoryapp/data/repo/AlumniRepo.kt
@@ -1,5 +1,6 @@
 package com.eeszen.alumnidirectoryapp.data.repo
 
+import com.eeszen.alumnidirectoryapp.data.model.Status
 import com.eeszen.alumnidirectoryapp.data.model.User
 import kotlinx.coroutines.flow.Flow
 
@@ -10,6 +11,11 @@ interface AlumniRepo {
     suspend fun addAlumni(user: User)
     suspend fun deleteAlumni(id:String)
     suspend fun updateAlumni(id:String,user: User)
+    suspend fun getApprovedAlumnis(): List<User>
+    suspend fun getPendingAlumnis(): List<User>
+    suspend fun getRejectedAlumnis(): List<User>
+
+    suspend fun updateUserStatus(id: String, status: Status)
 
     companion object {
         private var instance: AlumniRepo? = null
