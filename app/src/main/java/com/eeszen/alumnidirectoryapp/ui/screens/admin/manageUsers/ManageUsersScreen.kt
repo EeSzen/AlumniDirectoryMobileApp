420da24b9e6ae5c21dd0f1594eaf8d31eca26184
YeeLing <yeeling.wong@hotmail.com>
2026-01-09 13:50:28 +0800

    Add search and filter functionality to admin manage users screen

--- app/src/main/java/com/eeszen/alumnidirectoryapp/ui/screens/admin/manageUsers/ManageUsersScreen.kt
+++ app/src/main/java/com/eeszen/alumnidirectoryapp/ui/screens/admin/manageUsers/ManageUsersScreen.kt
@@ -10,14 +10,19 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterAlt
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -26,6 +31,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.eeszen.alumnidirectoryapp.ui.navigation.Screen
 import com.eeszen.alumnidirectoryapp.ui.screens.admin.composables.UserCard
+import com.eeszen.alumnidirectoryapp.ui.screens.dialog.BottomSheetDialog
 
 @Composable
 fun ManageUsersScreen(
@@ -34,7 +40,15 @@ fun ManageUsersScreen(
 ) {
     val users by viewModel.filteredUsers.collectAsStateWithLifecycle()
     val searchQuery by viewModel.searchQuery.collectAsStateWithLifecycle()
-
+    var showFilter by remember { mutableStateOf(false) }
+    if(showFilter) {
+        BottomSheetDialog(
+            onDismiss = { showFilter = false },
+            onApply = {
+                viewModel.onFiltersChanged(it)
+            }
+        )
+    }
     LaunchedEffect(Unit) {
         viewModel.getAllUsers()
     }
@@ -48,10 +62,11 @@ fun ManageUsersScreen(
             Row(
                 modifier = Modifier.fillMaxWidth().padding(16.dp)
             ) {
+                // Search
                 OutlinedTextField(
                     value = searchQuery,
                     onValueChange = {viewModel.onSearchChange(it)},
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.weight(0.7f),
                     placeholder = { Text("Search") },
                     leadingIcon = {
                         Icon(
@@ -61,6 +76,15 @@ fun ManageUsersScreen(
                     },
                     singleLine = true
                 )
+                // Filter
+                IconButton(
+                    onClick = { showFilter = true },
+                    modifier = Modifier.weight(0.3f)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.FilterAlt, ""
+                    )
+                }
             }
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
